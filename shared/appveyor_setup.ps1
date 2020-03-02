676b66a38f00a87d28d2d58aa0c2b30045c4deae
MSP-Greg <MSP-Greg@users.noreply.github.com>
2020-03-02 09:51:15 -0600

    appveyor_setup.ps1 - add 2.7.0 install ?

--- shared/appveyor_setup.ps1
+++ shared/appveyor_setup.ps1
@@ -343,8 +343,8 @@ function Install-Trunk {
   $two = $(&$trunk_exe -e "STDOUT.write RUBY_VERSION[/\A\d+\.\d+/].tr('.','').to_i - 1")
   $new_dir = "$dir_ruby$two$suf"
   if ( !(Test-Path -Path $new_dir -PathType Container) ) {
-    # Ruby 2.6.1
-    $t = $two[0] + "." + $two[1] + ".1-1"
+    # Ruby 2.7.0
+    $t = $two[0] + "." + $two[1] + ".0-1"
     Install-new $new_dir $t
   }
   return &$trunk_exe -e "STDOUT.write RUBY_VERSION[/\A\d+\.\d+/]"
